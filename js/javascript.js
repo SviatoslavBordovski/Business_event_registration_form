b0ea3840049ab88833157e2a361387744506c851
Sviatoslav Bordovski <sbordovski@gmail.com>
2019-07-15 21:37:26 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,9 @@
-function checkfirstName() {           
+function checkfirstName() {            
     var firstName = document.getElementById("firstName").value;
 	var rel = /^[a-zA-Z\s\'\-]{2,16}$/;
 
 	
-	if(rel.test(firstName)) {    
+	if(rel.test(firstName)) {
 		document.getElementById("firstNamePrompt").style.color="green"
 		document.getElementById("firstNamePrompt").innerHTML="<strong> Correct input</strong>";
 		return true;
